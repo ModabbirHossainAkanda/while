9039882c49fe66d88915f81cd226cba152e9cd8e
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-09 03:41:37 +0200

    using while-loop

---
+++ revision.js
@@ -0,0 +1,26 @@
+// string type variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+// number type variable
+var sunglassPrice = 120;
+var penColor ='blue';
+// boolean type variable
+var isExpensive = true;
+
+
+var tableItems = ['bottle', 'mouse', 'pen', 'sunglass', 'paper', 'pen'];
+// type 01
+// var item3 = tableItems[3];
+// console.log(item3);
+// type 2
+// tableItems[4] ='notebook';
+// console.log(tableItems);
+// tableItems[5] = 'glass';
+// console.log(tableItems);
+// type 3
+// var penIndex = tableItems.indexOf('pen');
+// console.log(penIndex);
+
+// type 04
+// push pop
+// conditionals
---
+++ while.js
@@ -0,0 +1,12 @@
+// var roastGiven = 0;
+// while(roastGiven <=7 ){
+//     console.log( 'Roast den , please !!')
+//     console.log(roastGiven);
+//     roastGiven++;
+// }
+
+    var num = 0;
+    while (num <= 10){
+        console.log('Hello');
+        num = num +1;
+    }
