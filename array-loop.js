5a3f53046e109f532f33f5b154d547b0ab6aa9ff
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-10 23:02:25 +0200

    array-loop

---
+++ array-loop.js
@@ -0,0 +1,14 @@
+// for(var i = 0; i<10; i++){
+//     console.log(i);
+// }
+// target: display every elements of an array
+// var numbers = [45, 87, 89, 80, 23, 40, 10, 2, 299, 388, 100, 25];
+// for(i=0; i<numbers.length; i++){
+//     var number = numbers[i];
+//     console.log(number);
+// }
+var items =['bottle', 'mouse', 'glass', 'headphone', 'laptop', 'bati', 'charger', 'multiplack', 'chair', 'notebook', 'wallet'];
+for(i = 0; i<items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
---
+++ for.js
@@ -0,0 +1,36 @@
+// var numbers = 0;
+// while(numbers < 100){
+//     console.log(numbers);
+//     numbers = numbers + 2;
+// }
+
+// var roastGiven = 0;
+// while(roastGiven < 7){
+//     console.log(roastGiven)
+//     roastGiven++;
+// }
+// for loop
+// for(var roastGiven = 0; roastGiven < 7; roastGiven++){
+//     console.log('Roast den , Please !!');
+// }
+
+// simple version of for loop
+// console.log('bou you are love of my love ');
+// for(var i = 0; i < 7; i++){
+//     console.log(i);
+// }
+
+// for(i=1; i<=10; i++){
+//     console.log(i);
+// }
+// for(i=0; i<20; i = i+2){
+//     console.log(i);
+// }
+// for(i=1; i<20; i=i+2){
+//     console.log(i);
+// }
+// var number = 0;
+// while(number <= 10){
+//     console.log(number);
+//     number++;
+// }
