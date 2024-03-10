9ca7d31f06aaf38ef7b97f66b0882992212744b0
ModabbirHossainAkanda <modabbirhossain041@gmail.com>
2024-03-10 02:59:56 +0200

    even odd loop

---
+++ numbers.js
@@ -0,0 +1,25 @@
+// // var number = 0 ; is called loop variable 
+// var number = 0;  
+// // condition inside while (number <10)             
+// while(number < 10){
+
+//     // loop body console.log(number)
+//    console.log(number);
+// //    change the loop variables 
+//     number++;
+// }
+
+// Target : Display 1 to 10
+
+var number = 1;
+while(number < 11){
+    console.log(number);
+    number = number + 1;
+}
+
+// Target : Display 1 to 20 
+var number = 1;
+while(number < 21){
+    console.log(number);
+    number = number +1;
+}
--- while.js
+++ while.js
@@ -4,9 +4,13 @@
 //     console.log(roastGiven);
 //     roastGiven++;
 // }
+// 1. Loop variable 
+//    2. Condition inside while 
+//    3. Loop body
+    // 4. Change the loop variable 
 
     var num = 0;
-    while (num <= 10){
+    while (num <= 10 ){
         console.log('Hello');
         num = num +1;
     }
